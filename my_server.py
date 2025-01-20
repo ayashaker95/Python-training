64a5567287c4e7507cc5bea4f2da5d0aba424d86
Aya Shaker <ashaker@asaltech.com>
2025-01-20 14:53:36 +0200

    new-changes

--- my_server.py
+++ my_server.py
@@ -1,35 +1,36 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__) # Initializes a new Flask application
 FILE_PATH = "db.txt"  # File to store data
 
 
-# GET API: Retrieves the content of the file
+# GET the content of the file
 @app.route('/file', methods=['GET'])
 def get_file_content():
     try:
+        # Open the file for read
         with open(FILE_PATH, 'r') as file:
             content = file.read()
-        return jsonify({"status": "success", "content": content}), 200
-    except FileNotFoundError:
-        return jsonify({"status": "error", "message": "File not found."}), 404
+            # Return the data 
+        return jsonify({"content": content})
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return jsonify({"message": str(e)})
 
 
-# POST API: Writes data to the file
+# POST data to the file
 @app.route('/file', methods=['POST'])
 def write_to_file():
     try:
-        data = request.json.get('data', None)
+        data = request.json.get('data', None) # Extract the data from the json 
+        # Condation if the data is NONE 
         if data is None:
-            return jsonify({"status": "error", "message": "Missing 'data' in request body."}), 400
-
+            return jsonify({"message": "Missing 'data' in request body."})
+        # open the file for write
         with open(FILE_PATH, 'w') as file:
-            file.write(data)
-        return jsonify({"status": "success", "message": "Data written to file."}), 200
+            file.write(data) # Write the the file 
+        return jsonify({"message": "Data written to file."})
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return jsonify({"message": str(e)})
 
 
 if __name__ == '__main__':
